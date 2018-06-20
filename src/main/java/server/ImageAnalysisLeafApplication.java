f19fe778166b43307bf95632ec6881042fec7a69
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2018-06-20 07:42:38 +0200

    Added storageservice

--- src/main/java/server/ImageAnalysisLeafApplication.java
+++ src/main/java/server/ImageAnalysisLeafApplication.java
@@ -1,12 +1,28 @@
 package server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import server.storageservice.StorageProperties;
+import server.storageservice.StorageService;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ImageAnalysisLeafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImageAnalysisLeafApplication.class, args);
 	}
+	
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
--- src/main/java/server/restcontroller/LeafController.java
+++ src/main/java/server/restcontroller/LeafController.java
@@ -1,13 +1,29 @@
 package server.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import server.imageanalysis.LeafTypes.LeafTypeName;
+import server.storageservice.StorageFileNotFoundException;
+import server.storageservice.StorageService;
 
 @RestController
 public class LeafController {
+	
+    private final StorageService storageService;
+
+    @Autowired
+    public LeafController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @RequestMapping("/leaftype")
     public LeafTypeName getLeafType(@RequestParam(value="name", defaultValue="World") String name) {
@@ -19,5 +35,20 @@ public class LeafController {
         return LeafTypeName.values();
     }
     
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+            RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
     
 }
